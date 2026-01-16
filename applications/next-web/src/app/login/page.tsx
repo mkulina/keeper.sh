55c2711e6c70d0f5ac62cc7d18177f669b8cab62
ridafkih <hello@rida.dev>
2026-01-16 07:28:28 -0500

    fix(next-web): dark mode login page ux

--- applications/next-web/src/app/login/page.tsx
+++ applications/next-web/src/app/login/page.tsx
@@ -20,10 +20,10 @@ const button = tv({
   `,
   variants: {
     variant: {
-      primary: "bg-primary text-primary-foreground hover:bg-primary-hover active:bg-primary-active",
-      secondary: "backdrop-brightness-95 hover:backdrop-brightness-90 active:backdrop-brightness-85 shadow-none",
-      border: "border-border hover:backdrop-brightness-95 active:backdrop-brightness-90",
-      ghost: "hover:backdrop-brightness-95 active:backdrop-brightness-90 shadow-none"
+      primary: "bg-primary text-primary-foreground hover:brightness-90 active:brightness-80 dark:hover:brightness-80 dark:active:brightness-70",
+      secondary: "text-foreground backdrop-brightness-95 hover:backdrop-brightness-90 active:backdrop-brightness-85 dark:backdrop-brightness-105 dark:hover:backdrop-brightness-150 dark:active:backdrop-brightness-175 shadow-none",
+      border: "border-border text-foreground hover:backdrop-brightness-95 active:backdrop-brightness-90 dark:hover:backdrop-brightness-150 dark:active:backdrop-brightness-175",
+      ghost: "text-foreground hover:backdrop-brightness-95 active:backdrop-brightness-90 dark:hover:backdrop-brightness-150 dark:active:backdrop-brightness-175 shadow-none"
     },
     size: {
       normal: "text-md px-4 py-2.5",
@@ -55,7 +55,7 @@ export const LinkButton: FC<WithButtonProps<ComponentProps<typeof Link>>> = ({ c
 
 export const Input: FC<WithButtonProps<DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>>> = ({ className, variant, size, ...props }) => {
   return (
-    <input {...props} className={cn("px-4 py-2.5 border-border rounded-xl border tracking-tight shadow-xs bg-surface-elevated focus-visible:outline-2 outline-offset-1 outline-border-emphasis disabled:opacity-50 disabled:cursor-not-allowed", className)}></input>
+    <input {...props} className={cn("px-4 py-2.5 border-border rounded-xl border tracking-tight shadow-xs bg-surface-elevated text-foreground focus-visible:outline-2 outline-offset-1 outline-border-emphasis disabled:opacity-50 disabled:cursor-not-allowed", className)}></input>
   )
 }
 
@@ -85,7 +85,7 @@ const headingFont = Lora()
 
 export const Heading1: FC<HTMLProps<HTMLHeadingElement>> = ({ className, children, ...props }) => {
   return (
-    <h1 {...props} className={cn(headingFont.className, "text-3xl tracking-tighter font-medium", className)}>{children}</h1>
+    <h1 {...props} className={cn(headingFont.className, "text-3xl tracking-tighter font-medium text-foreground", className)}>{children}</h1>
   )
 }
 
